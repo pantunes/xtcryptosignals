46d00e179f3083ae8ebbbaf87f50968ff9bff88e
Paulo Antunes <pjmlantunes@gmail.com>
2018-11-18 15:02:03 +0100

    Add __credits__ in Python files header

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,6 +1,6 @@
 __author__ = "Paulo Antunes"
 __copyright__ = "Copyright 2018, XTCryptoSignals"
-__credits__ = []
+__credits__ = ["Paulo Antunes", ]
 __license__ = "GPL"
 __version__ = "1.0"
 __maintainer__ = "Paulo Antunes"
