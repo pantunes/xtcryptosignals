1a33fa291c791889454297ebc4250f44550b5c45
Paulo Antunes <pjmlantunes@gmail.com>
2018-12-14 22:09:57 +0100

    Fixes in Python header/versioning

--- xtcryptosignals/__init__.py
+++ xtcryptosignals/__init__.py
@@ -1,1 +1,9 @@
+__title__ = "XTCryptoSignals"
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = ["Paulo Antunes", ]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
 __version__ = "0.0.2"
