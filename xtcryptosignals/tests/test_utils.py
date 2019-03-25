f1c7eeb5efeecb1fd7b5be1c3a610aad8c269036
Paulo Antunes <pjmlantunes@gmail.com>
2019-03-25 17:13:19 +0100

    Added empty test files for each component

---
+++ xtcryptosignals/tests/test_utils.py
@@ -0,0 +1,10 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = ["Paulo Antunes", ]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+def test_something():
+    assert True
