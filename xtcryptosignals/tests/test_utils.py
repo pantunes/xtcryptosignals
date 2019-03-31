bcec731c7ff40b8a45ebba3eabd9129d2418234f
Paulo Antunes <pjmlantunes@gmail.com>
2019-03-31 13:20:42 +0200

    Added some test cases

--- xtcryptosignals/tests/test_utils.py
+++ xtcryptosignals/tests/test_utils.py
@@ -6,5 +6,51 @@ __maintainer__ = "Paulo Antunes"
 __email__ = "pjmlantunes@gmail.com"
 
 
-def test_something():
-    assert True
+import pytest
+from xtcryptosignals.utils.helpers import (
+    get_class,
+    convert_to_seconds,
+)
+
+
+def test_get_class():
+    with pytest.raises(ModuleNotFoundError):
+        get_class('aaa', 'bbb')
+
+
+def test_convert_to_seconds():
+    with pytest.raises(TypeError):
+        convert_to_seconds(5)
+
+    with pytest.raises(TypeError):
+        convert_to_seconds(-1)
+
+    with pytest.raises(TypeError):
+        convert_to_seconds(None)
+
+    with pytest.raises(ValueError):
+        convert_to_seconds('12as')
+
+    assert convert_to_seconds('0s') == 0
+    assert convert_to_seconds('5s') == 5
+    assert convert_to_seconds('65s') == 65
+
+    assert convert_to_seconds('0m') == 0
+    assert convert_to_seconds('10m') == 600
+    assert convert_to_seconds('-10m') == -600
+
+    assert convert_to_seconds('0h') == 0
+    assert convert_to_seconds('10h') == 36000
+    assert convert_to_seconds('-10h') == -36000
+
+    assert convert_to_seconds('0d') == 0
+    assert convert_to_seconds('10d') == 864000
+    assert convert_to_seconds('-10d') == -864000
+
+    assert convert_to_seconds('0w') == 0
+    assert convert_to_seconds('10w') == 6048000
+    assert convert_to_seconds('-10w') == -6048000
+
+    assert convert_to_seconds('0y') == 0
+    assert convert_to_seconds('10y') == 290304000
+    assert convert_to_seconds('-10y') == -290304000
