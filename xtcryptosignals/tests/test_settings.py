ca612724b3d7260fe051d85a75787e4dccbe53cc
Paulo Antunes <pjmlantunes@gmail.com>
2019-03-31 13:34:06 +0200

    Added some test cases

--- xtcryptosignals/tests/test_settings.py
+++ xtcryptosignals/tests/test_settings.py
@@ -6,5 +6,18 @@ __maintainer__ = "Paulo Antunes"
 __email__ = "pjmlantunes@gmail.com"
 
 
-def test_something():
-    assert True
+from xtcryptosignals import settings as s
+from xtcryptosignals.utils.helpers import convert_to_seconds
+
+
+def test_ticker_schedule():
+    assert s.TICKER_SCHEDULE <= convert_to_seconds(s.HISTORY_FREQUENCY[0])
+
+
+def test_settings():
+    assert s.DEBUG
+    assert s.MONGODB_NAME
+    assert s.TICKER_SCHEDULE
+    assert s.TIMEOUT_PER_SYMBOL_REQUEST
+    assert s.TIMEOUT_PER_SYMBOLS_REQUEST
+    assert s.SYMBOL_FLOAT_PRECISION
