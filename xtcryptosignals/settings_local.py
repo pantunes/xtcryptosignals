884c047d18173e099cc9ba1ab82895a1c69cba59
pantunes <pjmlantunes@gmail.com>
2019-06-30 14:57:42 +0200

    Organized settings

--- xtcryptosignals/settings_local.py
+++ xtcryptosignals/settings_local.py
@@ -6,19 +6,5 @@ __maintainer__ = "Paulo Antunes"
 __email__ = "pjmlantunes@gmail.com"
 
 
-import os
-
-
-IP_ADDRESS = '0.0.0.0'
-PORT_SERVER = 5000
-PORT_CLIENT = 8000
-
-try:
-    SERVER_API_BASE_URL = os.environ['SERVER_API_BASE_URL']
-except KeyError:
-    # development
-    SERVER_API_BASE_URL = 'http://127.0.0.1:{}/'.format(PORT_SERVER)
-
-
 BINANCE_API_KEY = ''  # Add Personal Binance API KEY
 BINANCE_API_SECRET = ''  # Add Personal Binance API SECRET
