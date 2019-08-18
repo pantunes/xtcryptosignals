0cd86b7412cbe09db32222b0de9dfd946f8dcfa8
Paulo Antunes <pjmlantunes@gmail.com>
2019-08-18 17:15:44 +0200

    Templates fix

--- xtcryptosignals/settings_local.py
+++ xtcryptosignals/settings_local.py
@@ -8,3 +8,8 @@ __email__ = "pjmlantunes@gmail.com"
 
 BINANCE_API_KEY = ''  # Add Personal Binance API KEY
 BINANCE_API_SECRET = ''  # Add Personal Binance API SECRET
+
+CORS_ALLOWED_ORIGINS = (
+    'http://127.0.0.1:8000',
+    'https://xtcryptosignals.com',
+)
