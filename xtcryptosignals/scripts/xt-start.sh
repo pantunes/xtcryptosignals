66d3ab1f7a4af9788c4aceb94f0fa544a0a4902a
Paulo Antunes <pjmlantunes@gmail.com>
2019-10-20 12:01:41 +0200

    Updated GA_TRACKING_ID

--- xtcryptosignals/scripts/xt-start.sh
+++ xtcryptosignals/scripts/xt-start.sh
@@ -1,3 +1,3 @@
 nohup bash -c "DEBUG=0 xt-server" > /dev/null 2>&1 &
-nohup bash -c "GA_TRACKING_ID=UA-131351306-1 SERVER_API_BASE_URL=https://api.xtcryptosignals.com/ xt-client --prod" > /dev/null 2>&1 &
+nohup bash -c "GA_TRACKING_ID=UA-131351306-2 SERVER_API_BASE_URL=https://api.xtcryptosignals.com/ xt-client --prod" > /dev/null 2>&1 &
 nohup xt-ticker --enable-messaging --log-minimal > /dev/null 2>&1 &
