26461caea9257e014157591caf0002787385748b
Paulo Antunes <pjmlantunes@gmail.com>
2019-11-04 21:48:00 +0100

    Organized better JS code

--- xtcryptosignals/client/static/js/modals.js
+++ xtcryptosignals/client/static/js/modals.js
@@ -6,11 +6,22 @@ function setup_modals() {
             continue
         }
         let m = x.replace('#', '#modal_');
-        $(m).modal();
+
+        if (x === '#info') {
+            $(m).on(
+                $.modal.OPEN, function(event, modal) {
+                    $.get('/info', function(data) {
+                        $('#modal_info_text').html(data);
+                    });
+                });
+        }
+
         $(m).on(
             $.modal.AFTER_CLOSE, function(event, modal) {
                 location.hash = '';
         });
+
+        $(m).modal();
     }
 }
 
