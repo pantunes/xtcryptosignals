afecec0e16bea3fc04254ec6547d82f91c8070a8
Paulo Antunes <pjmlantunes@gmail.com>
2019-11-04 19:26:11 +0100

    Add JQuery-ui datepicker and more handlers/templates code implementation

---
+++ xtcryptosignals/client/static/js/notify/styles/bootstrap.js
@@ -0,0 +1,35 @@
+$.notify.addStyle("bootstrap", {
+    html: "<div>\n<span data-notify-text></span>\n</div>",
+    classes: {
+      base: {
+        "padding": "10px 17px 10px 16px",
+        "text-shadow": "0 1px 0 rgba(255, 255, 255, 0.5)",
+        "background-color": "#fcf8e3",
+        "border": "2px solid #fbeed5",
+        "border-radius": "4px",
+        "white-space": "nowrap",
+        "background-repeat": "no-repeat",
+        "background-position": "3px 7px"
+      },
+      error: {
+        "color": "#B94A48",
+        "background-color": "#F2DEDE",
+        "border-color": "#EED3D7",
+      },
+      success: {
+        "color": "#468847",
+        "background-color": "#DFF0D8",
+        "border-color": "#D6E9C6",
+      },
+      info: {
+        "color": "#3A87AD",
+        "background-color": "#D9EDF7",
+        "border-color": "#BCE8F1",
+      },
+      warn: {
+        "color": "#C09853",
+        "background-color": "#FCF8E3",
+        "border-color": "#FBEED5",
+      }
+    }
+  });
