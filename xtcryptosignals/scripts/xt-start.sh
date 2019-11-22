e7589e4007fa64026405ca516c9583010d97b05c
Paulo Antunes <pjmlantunes@gmail.com>
2019-11-22 20:41:03 +0100

    Add logos in MANIFEST.in

--- xtcryptosignals/scripts/xt-start.sh
+++ xtcryptosignals/scripts/xt-start.sh
@@ -1,3 +1,3 @@
 nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/server.prod.env xt-server" > /dev/null 2>&1 &
 nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/client.prod.env xt-client --production" > /dev/null 2>&1 &
-nohup SETTINGS_APP=`pwd`/config/server.prod.env xt-ticker --enable-messaging --log-minimal > /dev/null 2>&1 &
+nohup redis-cli FLUSHALL && SETTINGS_APP=`pwd`/config/server.prod.env xt-ticker --enable-messaging --log-minimal > /dev/null 2>&1 &
