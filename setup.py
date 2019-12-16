63d0dec775e17fab87e86359d0727c4bc2932371
Paulo Antunes <pjmlantunes@gmail.com>
2019-12-16 19:21:54 +0100

    Update pipelines to Py3.7

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
     ],
 )
