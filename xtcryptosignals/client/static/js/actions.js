c6d7d4eb537b6f0e83ae3a4d995af19cc5fe3375
Paulo Antunes <pjmlantunes@gmail.com>
2020-01-20 19:41:18 +0100

    add crypto fear & greed index

--- xtcryptosignals/client/static/js/actions.js
+++ xtcryptosignals/client/static/js/actions.js
@@ -91,3 +91,12 @@ function get_rules(handler) {
         process_fail(xhr);
     });
 }
+
+function get_crypto_fear_greed_index(handler) {
+    $.get('https://api.alternative.me/fng').done(function(response) {
+        handler(response)
+    })
+    .fail(function(xhr, status, error) {
+        process_fail(xhr);
+    });
+}
