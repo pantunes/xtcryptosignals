801abdd520f2053b705a560a7c7f68eb4ab75166
Paulo Antunes <pjmlantunes@gmail.com>
2020-03-19 21:39:30 +0100

    xt-start.sh small fix

--- xtcryptosignals/scripts/xt-start.sh
+++ xtcryptosignals/scripts/xt-start.sh
@@ -1,5 +1,13 @@
+# server instance (backend)
 nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/server.prod.env xt-server --port 5000" > /dev/null 2>&1 &
+
+# server instances (socket.io)
 nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/server.prod.env xt-server --port 5001" > /dev/null 2>&1 &
 nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/server.prod.env xt-server --port 5002" > /dev/null 2>&1 &
+nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/server.prod.env xt-server --port 5003" > /dev/null 2>&1 &
+
+# client instance (N workers will be spawn automatically based on cli param or number of cpus)
 nohup bash -c "FLASK_ENV=production SETTINGS_APP=`pwd`/config/client.prod.env xt-client --num-workers 2" > /dev/null 2>&1 &
+
+# ticker instance
 nohup bash -c "redis-cli FLUSHALL && SETTINGS_APP=`pwd`/config/server.prod.env xt-ticker --enable-messaging --log-minimal" > /dev/null 2>&1 &
