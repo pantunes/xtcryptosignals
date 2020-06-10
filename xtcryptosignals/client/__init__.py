8ac1a11a1fcdadfca6f35264ba117e4b995df041
Paulo Antunes <pjmlantunes@gmail.com>
2020-06-10 22:15:54 +0200

    Enforce Cookie default values

--- xtcryptosignals/client/__init__.py
+++ xtcryptosignals/client/__init__.py
@@ -16,7 +16,7 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(
     import_name=__name__,
     template_folder="templates",
-    # TODO @Note: let nginx or other more resourceful Load Balancer
+    # @Note: let nginx or other more resourceful Load Balancer
     # serve static content within this folder.
     static_folder="static",
 )
