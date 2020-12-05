324bf7feef1fc7edf38aa54bf0f5c06d855fe6d5
Paulo Antunes <pjmlantunes@gmail.com>
2020-12-05 20:51:01 +0000

    #147 Added Polkadot and Nucypher projects (#148)

---
+++ xtcryptosignals/server/migrations/20201205210000_add_nucypher_polkadot.py
@@ -0,0 +1,41 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = [
+    "Paulo Antunes",
+]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+from mongodb_migrations.base import BaseMigration
+from xtcryptosignals.common.utils import use_mongodb
+from xtcryptosignals.server.api.projects.models import Project
+from xtcryptosignals.tasks import settings as s
+
+
+PROJECTS = dict(
+    NU=dict(
+        name="Nucypher",
+        website="https://www.nucypher.com",
+        twitter="https://twitter.com/NuCypher",
+        wikipedia=None,
+    ),
+    DOT=dict(
+        name="Polkadot",
+        website="https://polkadot.network",
+        twitter="https://twitter.com/Polkadot",
+        wikipedia=None,
+    ),
+)
+
+
+class Migration(BaseMigration):
+    @use_mongodb(db=s.MONGODB_NAME, host=s.MONGODB_HOST, port=s.MONGODB_PORT)
+    def upgrade(self):
+        for k, v in PROJECTS.items():
+            Project(**{**{"coin_or_token": k}, **v}).save()
+            print(f"Adding Project: {k}")
+
+    def downgrade(self):
+        pass
