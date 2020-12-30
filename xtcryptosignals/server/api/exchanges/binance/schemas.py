d39eda5878fc493d00d3b6604c904055cf31c1fd
Paulo Antunes <pjmlantunes@gmail.com>
2020-12-30 15:47:13 +0100

    #196 Add other shell scripts to start server

--- xtcryptosignals/server/api/exchanges/binance/schemas.py
+++ xtcryptosignals/server/api/exchanges/binance/schemas.py
@@ -85,7 +85,7 @@ class ExchangeOpenOrdersOutputSchema(Schema):
             rows = BinanceAPI(
                 pkey=self.context["app"].config["SECRET_KEY"],
                 auth=self.context["auth"],
-            ).client.get_my_trades(symbol=data["symbol"], limit=30)
+            ).client.get_my_trades(symbol=data["symbol"], limit=200)
             rows.reverse()
             self.symbols.update({data["symbol"]: dict(rows=rows)})
         else:
