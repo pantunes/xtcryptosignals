8a87d6263db3430d68b6335068a0460adfdd8d70
Paulo Antunes <pjmlantunes@gmail.com>
2021-01-30 12:49:41 +0100

    Update Project Twitter handlers

---
+++ xtcryptosignals/server/migrations/20210130123800_update_twitter_handlers.py
@@ -0,0 +1,25 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = [
+    "Paulo Antunes",
+]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+from mongodb_migrations.base import BaseMigration
+from xtcryptosignals.common.utils import use_mongodb
+from xtcryptosignals.server.api.projects.models import Project
+from xtcryptosignals.tasks import settings as s
+
+
+class Migration(BaseMigration):
+    @use_mongodb(db=s.MONGODB_NAME, host=s.MONGODB_HOST, port=s.MONGODB_PORT)
+    def upgrade(self):
+        Project.objects(coin_or_token="UNI").update(twitter="https://twitter.com/Uniswap")
+        Project.objects(coin_or_token="LTO").update(twitter="https://twitter.com/TheLTONetwork")
+        Project.objects(coin_or_token="HBAR").update(twitter="https://twitter.com/hedera")
+
+    def downgrade(self):
+        pass
